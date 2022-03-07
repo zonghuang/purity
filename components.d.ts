d2cc5e6d21dc266ec5103cc8e592253665d5b392
zonghuang <1683766027@qq.com>
2022-03-07 09:26:19 +0800

    feat: ~

--- components.d.ts
+++ components.d.ts
@@ -20,7 +20,6 @@ declare module 'vue' {
     ElDialog: typeof import('element-plus/es')['ElDialog']
     ElForm: typeof import('element-plus/es')['ElForm']
     ElFormItem: typeof import('element-plus/es')['ElFormItem']
-    ElImage: typeof import('element-plus/es')['ElImage']
     ElInput: typeof import('element-plus/es')['ElInput']
     ElOption: typeof import('element-plus/es')['ElOption']
     ElRadio: typeof import('element-plus/es')['ElRadio']
