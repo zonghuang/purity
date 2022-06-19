bd4d6db3b9c718d3010dab2f7d4e1ca714513215
zonghuang <1683766027@qq.com>
2022-06-19 20:36:31 +0800

    ci: 1.删除.vscode 2.env

--- mock/sso/audit-log/sso-log.ts
+++ mock/sso/audit-log/sso-log.ts
@@ -1,6 +1,70 @@
 export const config = {
   name: 'sso-log',
   title: 'sso 日志',
-  elements: [],
+  elements: [
+    {
+        "uuid": "1655628263515",
+        "name": "zh-container",
+        "type": "root",
+        "style": {
+            "display": "block",
+            "minHeight": "60px"
+        },
+        "events": [],
+        "modelValue": "",
+        "propConfig": {},
+        "animations": [],
+        "childrens": [
+            {
+                "uuid": "1655628276985",
+                "name": "zh-input",
+                "type": "input",
+                "style": {},
+                "events": [],
+                "modelValue": "",
+                "propConfig": {
+                    "label": "输入框",
+                    "field": "",
+                    "placeholder": "请输入",
+                    "labelWidth": "100px",
+                    "labelPosition": "right"
+                },
+                "animations": []
+            },
+            {
+                "uuid": "1655628271229",
+                "name": "zh-form",
+                "type": "form",
+                "style": {
+                    "height": "100px"
+                },
+                "events": [],
+                "modelValue": {},
+                "propConfig": {
+                    "label": "表单",
+                    "formItemWidth": "",
+                    "labelWidth": "",
+                    "labelPosition": ""
+                },
+                "animations": [],
+                "childrens": []
+            },
+            {
+                "uuid": "1655628287541",
+                "name": "zh-button",
+                "type": "button",
+                "style": {
+                    "display": "inline-block"
+                },
+                "events": [],
+                "modelValue": "",
+                "propConfig": {
+                    "label": "按钮"
+                },
+                "animations": []
+            }
+        ]
+    }
+],
   settings: {},
 }
