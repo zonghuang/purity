fbda8572aa9015ced2ce8b8acf3bf81e7726680c
zonghuang <1683766027@qq.com>
2022-10-08 17:41:27 +0800

    feat: 导入 material 组件

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,28 @@ import store from './store'
 import { PurityComponents, elementPlustIcons } from './components'
 import PurityRender from './render'
 
+// button
 import '@material/web/button/elevated-button'
+import '@material/web/fab/fab-extended'
 import '@material/web/fab/fab'
+import '@material/web/button/filled-button'
+import '@material/web/button/tonal-button'
+import '@material/web/button/outlined-button'
+import '@material/web/button/outlined-link-button'
+import '@material/web/segmentedbutton/outlined-segmented-button'
+
+// checkbox
+import '@material/web/checkbox/checkbox'
+
+// radio
+import '@material/web/radio/radio'
+
+
+// text-field
 import '@material/web/textfield/filled-text-field'
 import '@material/web/textfield/outlined-text-field'
+
+// switch
 import '@material/web/switch/switch'
 
 import 'virtual:svg-icons-register'
