765fa407572b623c26cdbcb311b9eac699a5679b
zonghuang <1683766027@qq.com>
2022-10-08 17:34:16 +0800

    fix: getIn bug

--- src/utils/get-in.ts
+++ src/utils/get-in.ts
@@ -5,13 +5,11 @@ export const getIn = (data: IObject | IObject[], path?: string | string[]): any 
 
   // data 为数组
   if (Array.isArray(data)) {
-    return data.map((item: IObject) =>
-      Array.isArray(paths) ? getIn(item, paths) : item[paths]
-    )
+    return data.map((item: IObject) => getIn(item, paths))
   }
 
   // data 为对象
   return paths?.reduce((pre: IObject, cur: string) => {
-    return pre?.[cur]
+    return Array.isArray(pre) && cur ? getIn(pre, cur) : pre?.[cur]
   }, data)
 }
